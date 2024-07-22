b97b22a8b77b02799a72cc831dd9bfa707227d02
Bruno Cavalcante <bruno.cavalcante@stone.com.br>
2024-07-22 20:59:52 -0300

    feat: started stress test with goroutines, channels and waitGroups

--- cmd/stressTest.go
+++ cmd/stressTest.go
@@ -5,14 +5,16 @@ package cmd
 
 import (
 	"fmt"
+	"net/http"
+	"sync"
 
 	"github.com/spf13/cobra"
 )
 
 var (
 	url         string
-	qtyRequests int8
-	concurrency int8
+	qtyRequests int
+	concurrency int
 )
 
 // stressTestCmd represents the stressTest command
@@ -26,13 +28,43 @@ var stressTestCmd = &cobra.Command{
 	by Challenge from FullCycle Goexpert.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("stressTest called")
-		fmt.Println("URL:", url, "Number of requests:", qtyRequests, "Concurrency:", concurrency)
+
+		var wg sync.WaitGroup
+		concurrencyLimiter := make(chan struct{}, concurrency)
+
+		report := make(map[int]int)
+		var mu sync.Mutex
+
+		for range qtyRequests {
+			wg.Add(1)
+
+			concurrencyLimiter <- struct{}{}
+			go makeReq(url, report, &mu, &wg, concurrencyLimiter)
+		}
+
+		wg.Wait()
+		fmt.Println(report)
 	},
+}
+
+func makeReq(url string, report map[int]int, mu *sync.Mutex, wg *sync.WaitGroup, concurrencyLimiter chan struct{}) {
+	defer wg.Done()
+
+	res, _ := http.Get(url)
+	mu.Lock()
+	if _, exists := report[res.StatusCode]; !exists {
+		report[res.StatusCode] = 1
+	} else {
+		report[res.StatusCode]++
+	}
+	mu.Unlock()
+
+	<-concurrencyLimiter
 }
 
 func init() {
 	rootCmd.AddCommand(stressTestCmd)
 	stressTestCmd.Flags().StringVarP(&url, "url", "u", "http://google.com.br", "URL to execute test")
-	stressTestCmd.Flags().Int8VarP(&qtyRequests, "qtyRequests", "r", 5, "Number of requests")
-	stressTestCmd.Flags().Int8VarP(&concurrency, "concurrency", "c", 5, "Concurrency to execute test")
+	stressTestCmd.Flags().IntVarP(&qtyRequests, "qtyRequests", "r", 7, "Number of requests")
+	stressTestCmd.Flags().IntVarP(&concurrency, "concurrency", "c", 5, "Concurrency to execute test")
 }
